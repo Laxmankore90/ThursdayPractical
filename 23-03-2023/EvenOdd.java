3c5a2c0427327af1987d8598517364b443965444
Laxmankore90 <112858118+Laxmankore90@users.noreply.github.com>
2023-03-23 14:39:55 +0530

    Add files via upload

---
+++ 23-03-2023/EvenOdd.java
@@ -0,0 +1,12 @@
+class EvenOdd									// Creating a class to find even odd
+{
+	public static void main(String args[])		// main method
+	{
+		int num=66;							//data members
+		
+		if (num%2 == 0)							// condition
+			System.out.println(num+ "is Even");
+		else
+			System.out.println(num+ "is Odd");
+	}
+}
