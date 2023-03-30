fb34c3844215fcd0ef0a786e676634489287d59d
Laxman kore <112858118+Laxmankore90@users.noreply.github.com>
2023-03-31 00:27:13 +0530

    Update Main.java

--- 30-03-2023/Main.java
+++ 30-03-2023/Main.java
@@ -1,51 +1,61 @@
-// This is the Vehicle class
+									// This is the Vehicle class
  class Vehicle {
-    // These are the attributes of the Vehicle class
+						// These are the attributes of the Vehicle class
 		String brand;
 		String model;
 		int year;
 
-    // This is the constructor of the Vehicle class
-    public Vehicle(String brand, String model, int year) {
+														// This is the constructor of the Vehicle class
+     Vehicle(String brand, String model, int year) {
         this.brand = brand;
         this.model = model;
         this.year = year;
     }
 
-    // This is the drive method of the Vehicle class
-    public void drive() {
+								// This is the drive method of the Vehicle class
+     void drive() {
         System.out.println("Driving the vehicle...");
     }
 }
 
-// This is the Car class, which extends the Vehicle class
+															// This is the Car class, which extends the Vehicle class
  class Car extends Vehicle {
-    // This is the additional attribute of the Car class
+										// This is the additional attribute of the Car class
      String color;
 
-    // This is the constructor of the Car class
-    public Car(String brand, String model, int year, String color) {
-        // This calls the constructor of the parent class (Vehicle) to initialize its attributes
+														// This is the constructor of the Car class
+     Car(String brand, String model, int year, String color) {
+												// This calls the constructor of the parent class (Vehicle) to initialize its attributes
         super(brand, model, year);
-        // This initializes the color attribute of the Car class
+														// This initializes the color attribute of the Car class
         this.color = color;
     }
 
-    // This is the type method of the Car class
-    public void type() {
+													// This is the type method of the Car class
+     void type() {
         System.out.println("This is a car.");
+    }
+	public String toString() {
+        return "Brand: " + brand + ", \nModel: " + model + ", \nYear: " + year + ", \nColor: " + color;
+	}
+	 String getColor() {
+        return color;
     }
 }
 
-// This is the main class
-public class Main {
-    // This is the main method
+
+												// This is the main class
+ class Main {
+																// This is the main method
     public static void main(String[] args) {
-        // This creates an object of the Car class
+																// This creates an object of the Car class
         Car car = new Car("Toyota", "Camry", 2022, "Red");
-        // This calls the drive method inherited from the Vehicle class
+														// This calls the drive method inherited from the Vehicle class
         car.drive();
-        // This calls the type method specific to the Car class
+																						// This calls the type method specific to the Car class
         car.type();
+		
+		System.out.println(car);
+		System.out.println(car.getColor());
     }
 }
