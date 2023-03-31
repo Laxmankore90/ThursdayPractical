e9d539be7edbb51e312a96ebd1ca9ae488071a5b
Laxman kore <112858118+Laxmankore90@users.noreply.github.com>
2023-03-31 09:37:43 +0530

    Update Main.java

--- 30-03-2023/Main.java
+++ 30-03-2023/Main.java
@@ -5,57 +5,40 @@
 		String model;
 		int year;
 
-														// This is the constructor of the Vehicle class
-     Vehicle(String brand, String model, int year) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-    }
-
 								// This is the drive method of the Vehicle class
-     void drive() {
+     public void drive() {
         System.out.println("Driving the vehicle...");
     }
 }
 
 															// This is the Car class, which extends the Vehicle class
  class Car extends Vehicle {
-										// This is the additional attribute of the Car class
+										
      String color;
 
-														// This is the constructor of the Car class
-     Car(String brand, String model, int year, String color) {
-												// This calls the constructor of the parent class (Vehicle) to initialize its attributes
-        super(brand, model, year);
-														// This initializes the color attribute of the Car class
-        this.color = color;
-    }
-
-													// This is the type method of the Car class
-     void type() {
+     public void type() {
         System.out.println("This is a car.");
     }
-	public String toString() {
-        return "Brand: " + brand + ", \nModel: " + model + ", \nYear: " + year + ", \nColor: " + color;
-	}
-	 String getColor() {
-        return color;
-    }
+	
 }
 
 
 												// This is the main class
- class Main {
+ class Main01 {
 																// This is the main method
     public static void main(String[] args) {
 																// This creates an object of the Car class
-        Car car = new Car("Toyota", "Camry", 2022, "Red");
-														// This calls the drive method inherited from the Vehicle class
+        Car car = new Car();
+		
+		car.brand = "Toyota";
+		car.model = "Camry";
+		car.year = 2021;
+		car.color = "Red";	
+										
         car.drive();
-																						// This calls the type method specific to the Car class
+																					
         car.type();
 		
-		System.out.println(car);
-		System.out.println(car.getColor());
+		
     }
 }
